86d0df8437c8ad1ba575eaeb9f0c9df4733f65b8
Jose-de-la-Barra <jose.delabarra2@gmail.com>
2022-04-08 02:25:40 -0400

    quedó lista la estructura de personas. Se puede acceder a todos los datos de una manera muy facil. Es probable que en la línea 45 haya que agregar parte de la lógica de la tarea

--- main.c
+++ main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include<string.h>
+#include "Personas.h"
 
-int main() {
-    printf("Hello, World!\n");
+#define MAXCHAR 150
+
+int main(int argc, char *argv[]) {
+    FILE* fp;
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        return 1;
+    }
+
+    char row[MAXCHAR];
+    char *token;
+    int cont = 0;
+
+    // para sacar la primera línea
+    fgets(row, MAXCHAR, fp);
+    Persona  *personas = (Persona *) malloc(sizeof(Persona));
+
+
+    while (feof(fp) != true) {
+        fgets(row, MAXCHAR, fp);
+
+        Persona  *unaPersona = (Persona *) malloc(sizeof(Persona));
+
+
+        // Para conseguir el nombre
+        token = strtok(row, ",");
+        strcpy(unaPersona -> name, token);
+
+        // Para conseguir la categoría de peligrosidad
+        token = strtok(NULL, ",");
+        unaPersona -> dangerCategory = strtol(token, (char **) NULL, 0);
+
+        // Para conseguir la probabilidad de ataque
+        token = strtok(NULL, ",");
+        token = strtok(token, "\n");
+        if (token != NULL) {
+            unaPersona -> attackProb = strtold(token, (char **) NULL);
+        }
+        else unaPersona -> attackProb = 0;
+
+
+        personas[cont] = *unaPersona;
+        cont++;
+    }
+
+    printf("%s", personas[5].name);
+
     return 0;
 }
