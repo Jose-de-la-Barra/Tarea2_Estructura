dbb8d844e915f539967344cee475486f4752433b
Jose-de-la-Barra <jose.delabarra2@gmail.com>
2022-04-10 16:18:02 -0400

    Ya se rodenan los nombres por abecedario

--- main.c
+++ main.c
@@ -7,8 +7,10 @@
 #define MAXPERSONAS 200
 #define MAXCHAR 1000
 
-const char* PERSON_FORMAT_OUT = "%s, %ld, %Lf\n";
+const char* PERSON_FORMAT_OUT = "%s,%ld,%Lf,%ld\n";
 
+
+void sort_words(Persona* personas, int count, char *);
 void category(Persona* personas, int cont, char *, FILE *, char *);
 
 int main(int argc, char *argv[]) {
@@ -45,6 +47,8 @@ int main(int argc, char *argv[]) {
         }
         else unaPersona -> attackProb = -1;  // asignamos un -1 para la gente que no tiene probabilidad de ataque para diferenciar
 
+        unaPersona -> category = 0;  // agregamos una columna de ceros para un posterior ordenamiento
+
         personas[cont] = *unaPersona;
         cont++;
     }
@@ -57,20 +61,42 @@ int main(int argc, char *argv[]) {
 
 
 void category(Persona *personas, int cont, char* num_lista, FILE * fpin, char* arch_out) {
+    sort_words(personas, cont, arch_out);
 
-    FILE* fpout;
+    //FILE *fpout;
+    //fpout = fopen(arch_out, "w+");
+    //if (fpout == NULL) perror("Opening file");
+    //long int danger_category;
+
+    //for (int i = 0; i < cont; i++) {
+        //if (personas[i].dangerCategory <= 2 && personas[i].attackProb == (long double) -1) {
+        //    personas[i].category = 1;
+        //    fprintf(fpout, PERSON_FORMAT_OUT, personas[i].name, personas[i].dangerCategory, personas[i].attackProb,
+        //            personas[i].category);
+
+    //    fprintf(fpout, PERSON_FORMAT_OUT, personas[i].name, personas[i].dangerCategory, personas[i].attackProb,
+    //                personas[i].category);
+    //}
+
+    //fclose(fpout);
+}
+
+void sort_words(Persona* personas, int count, char* arch_out) {
+    FILE *fpout;
     fpout = fopen(arch_out, "w+");
     if (fpout == NULL) perror("Opening file");
-    long int danger_category;
+    char temp[100];
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j < count; j++) {
+            if (strcmp(personas[i].name, personas[j].name) < 0) {
 
-    for (int i = 0; i < cont; i++) {
-        danger_category = personas[i].dangerCategory;
-
-        if ( danger_category >= 4 ) {
-            fprintf(fpout, PERSON_FORMAT_OUT, personas[i].name, personas[i].dangerCategory, personas[i].attackProb);
+                strcpy(temp, personas[i].name);
+                strcpy(personas[i].name, personas[j].name);
+                strcpy(personas[j].name, temp);
+            }
         }
-
-        }
-
-    fclose(fpout);
+    }
+    for (int i = 0; i < count; i++) {
+        fprintf(fpout, PERSON_FORMAT_OUT, personas[i].name, personas[i].dangerCategory, personas[i].attackProb, personas[i].category);
+    }
 }
